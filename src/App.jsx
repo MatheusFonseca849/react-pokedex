cad833666b262cdc656e04fef568334ac05382a1
MatheusFonseca849 <matheusfonseca849@gmail.com>
2024-02-13 14:22:32 -0300

    update: created database state with all pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Header } from "./Components/Header"
 import { api } from "./services/api"
 
 function App() {
 
+  const [pokeList, setPokeList] = useState([])
+  const [pokeDatabase, setPokeDatabase] = useState([])
   useEffect(() => {
 
     async function loadPokemonData(){
@@ -11,21 +13,47 @@ function App() {
       try {
         
         const response = await api.get('pokemon', {
-          limit: 30,
-          offset: 0
+
+          params:{
+            limit: 30,
+            offset: 0,
+          }
         })
         console.log(response)
+
+        const database = await api.get('pokemon', {
+          params: {
+            limit: 10000,
+            offset: 0,
+          }
+        })
+        setPokeList(response.data.results)
+        setPokeDatabase(database.data.results)
       } catch (error) {
+
         console.log(error)
+      
       }
 
     }
     loadPokemonData()
+    console.log(pokeList)
+    console.log(pokeDatabase)
   }, [])
 
   return (
     <>
       <Header/>
+      {pokeList.map((pokemon) => {
+
+        return(
+          <li key={pokemon.name}>
+            <p>{pokemon.name}</p>
+
+          </li>
+        )
+
+      })}
     </>
   )
 }
